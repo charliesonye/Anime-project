18c8ccccb6f995f128634d2945437b25027b4034
Charles <c_onyemem@live.com>
2023-04-26 22:36:21 -0700

    Code needed for proper routing has been added to Navigation and App component

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,54 @@ import AnimeForm from './AnimeForm'
 import './App.css';
 
 function App() {
+  
+  const [score, setScore] = useState(0)
+  const [animes, setAnimes] = useState([])
+  const [searchText, setSearchText] = useState('')
+
+  useEffect(() => {
+    fetch('/animes')
+    .then(res => res.json())
+    .then(data => setAnimes(data))
+  }, [])
+
+  function addAnime(animes,newAnime){ 
+    setAnimes([...animes, newAnime])
+  }
+
+
+
+
   return (
     <div className="App">
-      
+      <Navigation />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br/>
+            <Switch >
+            
+              <Route exact path='/'>
+                <Quotes />
+              </Route>
+              <Route exact path='/animes' >
+                <Animes  
+                  animes={animes}
+                  searchText={searchText}
+                  setSearchText={setSearchText}  
+                  />
+              </Route>
+              <Route exact path='/animes/new' >
+                <AnimeForm addAnime={addAnime} animes={animes}  />
+              </Route>
+              <Route exact path= '/questions' >
+                <Questions  score={score} setScore={setScore} />  
+              </Route> 
+              <Route path='/questions/score' >
+                <ScoreDisplay  score={score} setScore={setScore}/>
+              </Route>
+            </Switch>
     </div>
   );
 }
--- client/src/Navigation.js
+++ client/src/Navigation.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
+
+const linkStyles = {
+    display: "inline-block",
+    fontFamily: 'Kongtext',
+    borderRadius: "25px",
+    width: "90px",
+    padding: "12px",
+    margin: "0 12px 6px",
+    background: "orange",
+    textDecoration: "none",
+    color: "white",
+  }; 
 
  function Navigation() {
-  return (
-    <div>Navigation</div>
+  
+  
+    return (
+   
+        <div className="NavBar">
+        <NavLink to='/'  style={linkStyles}>
+            Quotes
+        </NavLink>
+        <NavLink to='/animes'  style={linkStyles}>
+            Animes
+        </NavLink>
+        <NavLink to='/animes/new' style={linkStyles} >
+          Add Anime
+        </NavLink>
+        <NavLink to='/questions'  style={linkStyles}>
+            Anime Quiz
+        </NavLink>
+    </div>
+    
   )
 }
 
