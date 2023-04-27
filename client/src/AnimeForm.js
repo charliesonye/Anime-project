751d93554ecf5c5e67d0738d43ef54c46663f585
Charles <c_onyemem@live.com>
2023-04-26 22:43:35 -0700

    Added functionality to list array of animees and add new animes to array but need to pull data from server to complete function

--- client/src/AnimeForm.js
+++ client/src/AnimeForm.js
@@ -1,8 +1,70 @@
 import React from 'react'
+import {useHistory} from 'react-router-dom'
 
- function AnimeForm() {
+ function AnimeForm({ addAnime, animes}) {
+  
+  const history = useHistory()
+  const [text, setText] = useState('')
+  const [count, setCount] = useState(0)
+  const [formData, setFormData] =useState({
+    name: "",
+    image: "",
+    description: ""
+  })
+
+  function handleChange(e){
+    setFormData({
+      ...formData, 
+      [e.target.name]: e.target.value
+    })
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    const newAnime = {
+      name: formData.name,
+      image: formData.image,
+      description: formData.description
+    }
+
+    fetch("http://localhost:3000/animes", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newAnime)
+    })
+    .then(res => res.json())
+    .then(data => {
+      addAnime(animes, data)
+      setFormData({
+        name: "",
+        image: "",
+        description: ""
+      })
+      
+      history.push('/animes')
+    })
+   
+    
+  }
+
   return (
-    <div>AnimeForm</div>
+    <div  className='AnimeForm'>
+        <h2>Add one Anime</h2> 
+        <hr />
+        <form onSubmit={handleSubmit}>
+            <label>Input Name: </label>
+            <textarea type='text' name='name' value={formData.name} onChange={handleChange}/>
+            <label>Input Image: </label>
+            <textarea type='text' name='image' value={formData.image} onChange={handleChange} /><br />
+            <label>Input Synopsis: </label>
+            <textarea type='text' name='description'  value={formData.description} onChange={handleChange}/><br />
+
+            <input type='submit' />
+        </form>
+    
+    </div>
   )
 }
 
--- client/src/Animes.js
+++ client/src/Animes.js
@@ -1,8 +1,68 @@
 import React from 'react'
+import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa'
 
-function Animes() {
+
+
+function Animes({ animes, searchText, setSearchText}) { 
+  
+  const [current, setCurrent] = useState(0)
+
+
+  const previousClick = () => {
+    
+    setCurrent( current === 0 ? animes.length -1: current -1)
+  }
+
+  const nextClick = () => {
+    setCurrent(current ===  animes.length - 1? 0: current + 1)
+  }
+
+  const filteredAnimes = [...animes]?.filter((anime)=> {
+    if (searchText === '') {
+      return anime
+    } else if (anime.name.toLowerCase().includes(searchText.toLowerCase())) {
+      return anime
+    }
+    return false
+}) 
+  
   return (
-    <div>Animes</div>
+    <div>
+      <input 
+        className='animeSearchBar'
+        placeholder='Enter Anime Name'
+        onChange={(e)=> setSearchText(e.target.value)}
+        />
+      <div className='carousel' >
+      <FaArrowAltCircleLeft className='leftArrow' onClick={() => previousClick()} />
+      <FaArrowAltCircleRight className='rightArrow' onClick={() => nextClick()} />
+      
+      { 
+
+        filteredAnimes.map((anime, index) =>  (
+      
+          <div key={anime.name} className={index === current? 'smoothTransition': 'transition'}>
+           
+              {index === current && (
+               <>             
+                
+                <img  src={anime.image} alt="The anime" className='image' />
+                <h2> Name: {anime.name}</h2>
+                <hr />
+                <h3>Show Description</h3>
+                <small>{anime.description}</small>
+               </>
+              )}
+          </div>
+      ))
+            
+      }
+      
+      
+    </div>
+
+    </div>
+    
   )
 }
 
