61b33222f3b2684ca17cb60e71bb6c067de0c98c
Charles <c_onyemem@live.com>
2023-04-26 23:04:40 -0700

    Quotes component can now pull random quotes from external api

--- client/src/Quotes.js
+++ client/src/Quotes.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 
- function Quotes() {
+ function Quotes({PageTitleStyle}) {
+ 
+  const [quote, setQuote] = useState('')
+  const [newQuote, setNewQuote] = useState(false)
+
+  useEffect(()=> {
+    fetch('https://animechan.vercel.app/api/random')
+    .then(res => res.json())
+    .then(data => setQuote(data) )
+  }, [newQuote])
+
+  function changeQuote(){
+    setNewQuote(!newQuote)
+  }
   return (
-    <div>Quotes</div>
+    <div className='Quotes'>
+        <h2 style={PageTitleStyle}>Quotes from your favorite anime characters! </h2>
+        <hr />
+        <h3>"{quote.quote}"</h3>
+        <hr />
+        <label>Character: {quote.character}</label>
+        <hr />
+        <br />
+        <br />
+        <label>Anime: {quote.anime}</label>
+        <hr />
+        <button onClick={changeQuote}>New Quote</button>
+    </div>
   )
 }
 
